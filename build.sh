5438094a79e2518fd95a2097037f6eada81dafed
CobbCoding <rircobb@gmail.com>
2023-09-30 20:04:49 -0400

    Fixed collision issue, and added a scoring system with text which gets displayed to the screen pog

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 set -xe
-gcc main.c -o main -I /usr/include/SDL -lSDL2 -lSDL2_image
+gcc main.c -o main -I /usr/include/SDL -lfreetype -lSDL2_ttf -lSDL2 -lSDL2_image -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/harfbuzz -I/usr/lib/glib-2.0/include -I/usr/include/sysp
--- main.c
+++ main.c
@@ -5,16 +5,53 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 int WIDTH = 1280;
 int HEIGHT = 720;
+
+typedef struct{
+    SDL_Rect rect;
+    int score;
+} Paddle;
 
 typedef struct{
     int speed_x;
     int speed_y;
 } BallSpeed;
 
+bool handle_ball_bouncing_off_paddle(SDL_Rect ball, SDL_Rect paddle){
+    if((ball.x + ball.w) > (paddle.x) && (ball.x) < (paddle.x + paddle.w) && (ball.y + ball.h) > (paddle.y) && (ball.y) < (paddle.y + paddle.h)){
+        return true;
+    }
+
+    return false;
+}
+
+void get_text_and_rect(SDL_Renderer *renderer, int x, int y, char *text, TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect){
+    int text_width;
+    int text_height;
+
+    SDL_Surface *surface;
+    SDL_Color textColor = {255, 255, 255, 0};
+
+    surface = TTF_RenderText_Solid(font, text, textColor);
+    *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    text_width = surface->w;
+    text_height = surface->h;
+    SDL_FreeSurface(surface);
+    rect->x = x;
+    rect->y = y;
+    rect->w = text_width;
+    rect->h = text_height;
+}
+
 int main(){
+
+    // SDL INIT
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
         printf("ERROR: Could not initialize: %s\n", SDL_GetError());
     }
@@ -24,6 +61,20 @@ int main(){
 
     SDL_Renderer *rend = SDL_CreateRenderer(win, -1, render_flags);
 
+    // SDL TTF
+    TTF_Init();
+    TTF_Font *font = TTF_OpenFont("/usr/share/fonts/ubuntu/UbuntuMono-R.ttf", 64);
+    if(!font){
+        fprintf(stderr, "ERROR: Could not load font\n");
+        exit(1);
+    }
+
+    SDL_Rect word1;
+    SDL_Rect word2;
+    SDL_Texture *word_texture1;
+    SDL_Texture *word_texture2;
+
+
     SDL_Surface *surface;
     surface = IMG_Load("texture.jpeg");
 
@@ -31,27 +82,29 @@ int main(){
 
     SDL_FreeSurface(surface);
 
-    SDL_Rect player;
-    SDL_Rect dest2;
+    Paddle player;
+    player.score = 0;
+    Paddle dest2;
+    dest2.score = 0;
     SDL_Rect ball;
 
-    SDL_QueryTexture(tex, NULL, NULL, &player.w, &player.h);
-    SDL_QueryTexture(tex, NULL, NULL, &dest2.w, &dest2.h);
+    SDL_QueryTexture(tex, NULL, NULL, &player.rect.w, &player.rect.h);
+    SDL_QueryTexture(tex, NULL, NULL, &dest2.rect.w, &dest2.rect.h);
     SDL_QueryTexture(tex, NULL, NULL, &ball.w, &ball.h);
 
     // Initialize player size and location
-    player.w /= 8;
-    player.h /= 2;
+    player.rect.w /= 8;
+    player.rect.h /= 2;
 
-    player.x = 0 + 20;
-    player.y = (HEIGHT - player.h) / 2;
+    player.rect.x = 0 + 20;
+    player.rect.y = (HEIGHT - player.rect.h) / 2;
 
     // Initialize enemy size and location
-    dest2.w /= 8;
-    dest2.h /= 2;
+    dest2.rect.w /= 8;
+    dest2.rect.h /= 2;
 
-    dest2.x = (WIDTH - dest2.w) - 20;
-    dest2.y = (HEIGHT - dest2.h) / 2;
+    dest2.rect.x = (WIDTH - dest2.rect.w) - 20;
+    dest2.rect.y = (HEIGHT - dest2.rect.h) / 2;
 
     // Initialize ball size and location 
     ball.w /= 8;
@@ -62,7 +115,7 @@ int main(){
 
     bool close = false;
 
-    int speed = 800;
+    int speed = 1000;
     BallSpeed ball_speed;
     ball_speed.speed_x = 300;
     ball_speed.speed_y = 0;
@@ -72,6 +125,14 @@ int main(){
         ball.y += ball_speed.speed_y / 30;
         SDL_Event event;
 
+
+        char *player_score = malloc(sizeof(char) * 4);
+        char *enemy_score = malloc(sizeof(char) * 4);
+        sprintf(player_score, "%d", player.score);
+        sprintf(enemy_score, "%d", dest2.score);
+        get_text_and_rect(rend, (WIDTH/2) - 100, 0, player_score, font, &word_texture1, &word1);
+        get_text_and_rect(rend, (WIDTH/2) + 100, 0, enemy_score, font, &word_texture2, &word2);
+
         while(SDL_PollEvent(&event)){
             switch(event.type){
                 case SDL_QUIT:
@@ -80,12 +141,10 @@ int main(){
                 case SDL_KEYDOWN:
                     switch(event.key.keysym.scancode){
                         case SDL_SCANCODE_W:
-                            player.y -= speed / 30;
-                            dest2.y -= speed / 30;
+                            player.rect.y -= speed / 30;
                             break;
                         case SDL_SCANCODE_S:
-                            player.y += speed / 30;
-                            dest2.y += speed / 30;
+                            player.rect.y += speed / 30;
                             break;
                         default:
                             break;
@@ -93,40 +152,53 @@ int main(){
             }
         }
 
+        if(dest2.rect.y < ball.y){
+            dest2.rect.y += speed / 300;
+        }
+
+        if(dest2.rect.y > ball.y){
+            dest2.rect.y -= speed / 300;
+        }
+
+
         // Clamp player boundries
-        if(player.x + player.w > WIDTH){
-            player.x = WIDTH - player.w;
+        if(player.rect.x + player.rect.w > WIDTH){
+            player.rect.x = WIDTH - player.rect.w;
         }
 
-        if(player.x < 0){
-            player.x = 0;
+        if(player.rect.x < 0){
+            player.rect.x = 0;
         }
 
-        if(player.y + player.h > HEIGHT){
-            player.y = HEIGHT - player.h;
+        if(player.rect.y + player.rect.h > HEIGHT){
+            player.rect.y = HEIGHT - player.rect.h;
         }
 
-        if(player.y < 0){
-            player.y = 0;
+        if(player.rect.y < 0){
+            player.rect.y = 0;
         }
 
         // handle ball bouncing
-        if(ball.x + ball.w > WIDTH || (ball.x + ball.w) < (player.w + player.x)){
+        if(ball.x + ball.w > WIDTH){
             ball_speed.speed_x = -ball_speed.speed_x;
+            player.score += 1;
+            printf("Player: %d, Enemy: %d\n", player.score, dest2.score);
         }
 
-        if((ball.x - ball.w) < (player.w - player.x) && ((ball.y - ball.h) > (player.y - player.h) && (ball.y + ball.h) < (player.y + player.h))){
+        if(handle_ball_bouncing_off_paddle(ball, player.rect)){
             ball_speed.speed_x = -ball_speed.speed_x;
-            ball_speed.speed_y = 300; 
+            ball_speed.speed_y = 300;
         }
 
-        if(((ball.x + ball.w) > (dest2.w + dest2.x)-ball.w) && ((ball.y - ball.h) < (dest2.y + dest2.h) + dest2.h) && ((ball.y + ball.h) > (dest2.y + dest2.h) - dest2.h)){
+        if(handle_ball_bouncing_off_paddle(ball, dest2.rect)){
             ball_speed.speed_x = -ball_speed.speed_x;
-            ball_speed.speed_y = -300; 
+            ball_speed.speed_y = -300;
         }
-
+        
         if(ball.x < 0){
             ball_speed.speed_x = -ball_speed.speed_x;
+            dest2.score += 1;
+            printf("Player: %d, Enemy: %d\n", player.score, dest2.score);
         }
 
         if(ball.y + ball.h > HEIGHT){
@@ -137,15 +209,23 @@ int main(){
             ball_speed.speed_y = -ball_speed.speed_y;
         }
 
+
+        SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
+        SDL_RenderPresent(rend);
         SDL_RenderClear(rend);
-        SDL_RenderCopy(rend, tex, NULL, &player);
-        SDL_RenderCopy(rend, tex, NULL, &dest2);
+        SDL_RenderCopy(rend, tex, NULL, &player.rect);
+        SDL_RenderCopy(rend, tex, NULL, &dest2.rect);
         SDL_RenderCopy(rend, tex, NULL, &ball);
+        SDL_RenderCopy(rend, word_texture1, NULL, &word1);
+        SDL_RenderCopy(rend, word_texture2, NULL, &word2);
         SDL_RenderPresent(rend);
         SDL_Delay(1000 / 60);
     }
 
+
+
     SDL_DestroyTexture(tex);
+    SDL_DestroyTexture(word_texture1);
     SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
     SDL_Quit();
