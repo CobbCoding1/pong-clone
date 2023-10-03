bbbf03f6749bffca141c7da81862fe70b5fb9289
CobbCoding <rircobb@gmail.com>
2023-10-03 14:36:36 -0400

    Added game reset and net texture displayed in the middle of the screen

--- main.c
+++ main.c
@@ -51,15 +51,24 @@ void get_text_and_rect(SDL_Renderer *renderer, int x, int y, char *text, TTF_Fon
 
 int main(){
 
+    Paddle player;
+    player.score = 0;
+    Paddle dest2;
+    dest2.score = 0;
+    SDL_Rect ball;
+
+
     // SDL INIT
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
         printf("ERROR: Could not initialize: %s\n", SDL_GetError());
     }
     SDL_Window *win = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-
     Uint32 render_flags = SDL_RENDERER_ACCELERATED;
 
+_reset:
     SDL_Renderer *rend = SDL_CreateRenderer(win, -1, render_flags);
+
+
 
     // SDL TTF
     TTF_Init();
@@ -74,23 +83,31 @@ int main(){
     SDL_Texture *word_texture1;
     SDL_Texture *word_texture2;
 
+    SDL_Rect net_rect;
 
     SDL_Surface *surface;
+    SDL_Surface *net_surface;
     surface = IMG_Load("texture.jpeg");
+    net_surface = IMG_Load("net.png");
 
     SDL_Texture *tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_Texture *net_tex = SDL_CreateTextureFromSurface(rend, net_surface);
 
     SDL_FreeSurface(surface);
-
-    Paddle player;
-    player.score = 0;
-    Paddle dest2;
-    dest2.score = 0;
-    SDL_Rect ball;
+    SDL_FreeSurface(net_surface);
 
     SDL_QueryTexture(tex, NULL, NULL, &player.rect.w, &player.rect.h);
     SDL_QueryTexture(tex, NULL, NULL, &dest2.rect.w, &dest2.rect.h);
     SDL_QueryTexture(tex, NULL, NULL, &ball.w, &ball.h);
+
+
+    SDL_QueryTexture(net_tex, NULL, NULL, &net_rect.w, &net_rect.h);
+
+
+    net_rect.w = 20;
+    net_rect.h = HEIGHT;
+    net_rect.x = WIDTH / 2 + 10;
+    net_rect.y = 0;
 
     // Initialize player size and location
     player.rect.w /= 8;
@@ -114,6 +131,7 @@ int main(){
     ball.y = (HEIGHT - ball.h) / 2;
 
     bool close = false;
+    bool reset = false;
 
     int speed = 1000;
     BallSpeed ball_speed;
@@ -124,6 +142,11 @@ int main(){
         ball.x += ball_speed.speed_x / 30;
         ball.y += ball_speed.speed_y / 30;
         SDL_Event event;
+
+        if(player.score >= 11 || dest2.score >= 11){
+            reset = false;
+            break;
+        }
 
 
         char *player_score = malloc(sizeof(char) * 4);
@@ -183,6 +206,8 @@ int main(){
             ball_speed.speed_x = -ball_speed.speed_x;
             player.score += 1;
             printf("Player: %d, Enemy: %d\n", player.score, dest2.score);
+            reset = true;
+            break;
         }
 
         if(handle_ball_bouncing_off_paddle(ball, player.rect)){
@@ -199,6 +224,8 @@ int main(){
             ball_speed.speed_x = -ball_speed.speed_x;
             dest2.score += 1;
             printf("Player: %d, Enemy: %d\n", player.score, dest2.score);
+            reset = true;
+            break;
         }
 
         if(ball.y + ball.h > HEIGHT){
@@ -218,6 +245,7 @@ int main(){
         SDL_RenderCopy(rend, tex, NULL, &ball);
         SDL_RenderCopy(rend, word_texture1, NULL, &word1);
         SDL_RenderCopy(rend, word_texture2, NULL, &word2);
+        SDL_RenderCopy(rend, net_tex, NULL, &net_rect);
         SDL_RenderPresent(rend);
         SDL_Delay(1000 / 60);
     }
@@ -226,8 +254,16 @@ int main(){
 
     SDL_DestroyTexture(tex);
     SDL_DestroyTexture(word_texture1);
+    SDL_DestroyTexture(word_texture2);
+    SDL_DestroyTexture(net_tex);
     SDL_DestroyRenderer(rend);
+
+    if(reset){
+        goto _reset;
+    }
+
     SDL_DestroyWindow(win);
     SDL_Quit();
+
     return 0;
 }
